4e68aab9e1e6fb60824ec05535e7dfe9b9e5d925
Lorenzo <lorenzo@lorenzo-VirtualBox.(none)>
2014-11-14 21:02:48 -0500

    Hotfix

--- src/net.cpp
+++ src/net.cpp
@@ -1192,7 +1192,7 @@ void MapPort(bool)
 // The first name is used as information source for addrman.
 // The second name should resolve to a list of seed addresses.
 static const char *strMainNetDNSSeed[][2] = {
-{"107.170.240.166", "107.170.240.166"}
+{NULL, NULL}
 };
 
 static const char *strTestNetDNSSeed[][2] = {
